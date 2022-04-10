5a2c19d5edbae04d76caa60435aed001d8456d46
Shiping Li <lslsp@msn.com>
2022-04-10 16:40:17 +0800

    update for module8

--- main.go
+++ main.go
@@ -6,21 +6,37 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"strings"
+	//"github.com/golang/glog"
 )
 
 func main() {
+	//flag.Set("v", "4")
+	//glog.V(2).Info("Starting http server...")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
-	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
 	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("entering root handler")
+	user := r.URL.Query().Get("user")
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
 	// 1.接收客户端request，并将requst中带的header写入reponse header
 	io.WriteString(w, "===================Details of the http request header:============\n")
 	for k, v := range r.Header {
@@ -56,7 +72,8 @@ func ClientIP(r *http.Request) string {
 	return ""
 }
 
-func healthz(w http.ResponseWriter, r *http.Request) {
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	//4.当访问localhost/healthz时，应返回200
-	fmt.Fprintf(w, "200")
+	log.Printf("entering healthz handler")
+	io.WriteString(w, "The HttpServer is Ready.\n")
 }
